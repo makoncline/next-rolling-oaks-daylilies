8389f197374bca6e3abd28c71d328cf5100178b9
Makon <makon@hey.com>
2023-02-25 13:07:16 -0500

    dont use webp

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
       "images.daylilycatalog.com",
       "www.daylilydatabase.org",
     ],
+    format: ["image/png", "image/jpeg"],
   },
   experimental: {
     externalDir: true,
